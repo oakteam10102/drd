de2c5030d3669cdd9c7940c222f075c4f9c941cc
Taufiq Muhammadi <waruboy@yahoo.com>
2014-09-05 05:31:11 +0700

    search by zipcode functionality

--- app/controllers/search_pages_controller.rb
+++ app/controllers/search_pages_controller.rb
@@ -5,5 +5,9 @@ class SearchPagesController < ApplicationController
   def search_result
     @procedure = Procedure.find(params[:procedure][:procedure_id])
     @practices = @procedure.practices.all
+    @zipcode = params[:zipcode]
+    unless  @zipcode.blank?
+      @practices = @practices.where(zipcode: @zipcode)
+    end
   end
 end
