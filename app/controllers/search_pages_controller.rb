2cfc00631b440a40ccba76449e6bf911a57e48b9
Taufiq Muhammadi <waruboy@yahoo.com>
2014-09-06 06:12:58 +0700

    display sorting button

--- app/controllers/search_pages_controller.rb
+++ app/controllers/search_pages_controller.rb
@@ -1,13 +1,22 @@
 class SearchPagesController < ApplicationController
+  SORTING_OPTIONS = [['Sort By',""],['Price', 'Price'],['Distance','Distance'],['Review','Review']]
   def search
+    @sorting_options = SORTING_OPTIONS
   end
 
   def search_result
+    @sorting_options = SORTING_OPTIONS
     @procedure = Procedure.find(params[:procedure][:procedure_id])
     @practices = @procedure.practices.all
     @zipcode = params[:zipcode]
     unless  @zipcode.blank?
       @practices = @practices.where(zipcode: @zipcode)
+    end
+    @sort_by = params[:sort_by]
+    unless @sort_by.blank?
+      case @sort_by
+      when "Price"
+      end
     end
   end
 end
