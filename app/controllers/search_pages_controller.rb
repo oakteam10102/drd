3f886fdc5c21c7282875d76a4599b173f0206dbc
Taufiq Muhammadi <waruboy@yahoo.com>
2014-09-13 17:05:20 +0700

    10 search results per page

--- app/controllers/search_pages_controller.rb
+++ app/controllers/search_pages_controller.rb
@@ -39,7 +39,7 @@ class SearchPagesController < ApplicationController
     @qualification_filter = params[:qualifications]
     @costs = @costs.by_practice_name(@name_filter).by_price(@price_filter).
     by_rating(@rating_filter).by_qualification(@qualification_filter).
-    page(params[:page]).per(2)
+    page(params[:page]).per(10)
 
     @resorting_path = search_pages_search_result_path(
       procedure: {procedure_id: procedure_id}, 
